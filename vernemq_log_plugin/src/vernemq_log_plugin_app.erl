cf88b3204c872925e61e31d14ff6e02dc47601fe
Nobuhiro tada <s16a1041@bene.fit.ac.jp>
2019-10-10 06:00:03 +0000

    plugin名前を変更し　フォルダを追加しました

---
+++ vernemq_log_plugin/src/vernemq_log_plugin_app.erl
@@ -0,0 +1,16 @@
+-module(vernemq_log_plugin_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    vernemq_log_plugin_sup:start_link().
+
+stop(_State) ->
+    ok.
